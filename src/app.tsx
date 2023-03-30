f8bef7cad01f1266854577bcf7ec2c9d3ed3f7f2
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-03-30 23:59:23 +0800

    Init Project

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const App: FC = () => {
+    return <div className="text-red-500">Hello React 18</div>;
+};
+
+export default App;
---
+++ webpack/webpack.base.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, "..", "dist"),
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./template/index.html",
+        }),
+    ],
+};
