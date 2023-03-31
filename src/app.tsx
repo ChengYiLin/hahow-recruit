9cf0294c14d6e3e952d1c2b35ce69a4c607d48a5
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-03-31 15:49:12 +0800

    feat: Add Reset CSS to the project

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,18 @@
 import { FC } from "react";
 import styled from "styled-components";
+import ResetCSS from "./styles/resetCSS";
 
 const Title = styled.div`
     color: red;
 `;
 
 const App: FC = () => {
-    return <Title>Hello React 18</Title>;
+    return (
+        <>
+            <ResetCSS />
+            <Title>Hello React 18</Title>
+        </>
+    );
 };
 
 export default App;
