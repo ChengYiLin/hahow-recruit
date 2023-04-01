151949045f712bc2177cb6a66ecccffc6c2a166d
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-04-02 04:06:26 +0800

    feat: Add styled-component theme provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,17 @@
 import { FC } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import ResetCSS from "./styles/resetCSS";
+import theme from "./styles/theme";
 import AppRoutes from "./routes";
 
 const App: FC = () => {
     return (
         <BrowserRouter>
             <ResetCSS />
-            <AppRoutes />
+            <ThemeProvider theme={theme}>
+                <AppRoutes />
+            </ThemeProvider>
         </BrowserRouter>
     );
 };
