e2e365068894704c445e7dda6928d76a207ca6cc
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-04-01 14:33:45 +0800

    feat: Add Fetch API Logic to pages

--- src/pages/Hero/index.tsx
+++ src/pages/Hero/index.tsx
@@ -1,20 +1,41 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { getHeroesList } from "../../services";
+import { IHeroInfo } from "../../types/service";
 
 const Hero: FC = () => {
+    const [heroList, setHeroList] = useState<IHeroInfo[]>([]);
+
+    useEffect(() => {
+        getHeroesList()
+            .then((data) => {
+                Array.isArray(data) ? setHeroList(data) : setHeroList([]);
+            })
+            .catch((error) => {
+                if (error?.message) {
+                    alert(error.message);
+                } else {
+                    console.error(error);
+                }
+            });
+    }, []);
+
     return (
         <div>
-            Hero
-            <ul>
-                <li>
-                    <Link to="/hero/1">1</Link>
-                </li>
-                <li>
-                    <Link to="/hero/2">2</Link>
-                </li>
-                <li>
-                    <Link to="/hero/3">3</Link>
-                </li>
+            <ul style={{ display: "flex" }}>
+                {heroList.map((heroInfo) => (
+                    <li key={heroInfo.id}>
+                        <Link to={`/hero/${heroInfo.id}`}>
+                            <div>
+                                <img
+                                    src={`${heroInfo.image}`}
+                                    alt={`${heroInfo.name}`}
+                                />
+                                <p>{heroInfo.name}</p>
+                            </div>
+                        </Link>
+                    </li>
+                ))}
             </ul>
             <Outlet />
         </div>
--- src/pages/HeroDetail/index.tsx
+++ src/pages/HeroDetail/index.tsx
@@ -1,10 +1,76 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getHeroProfile, updateHeroProfile } from "../../services";
+import { IHeroProfile } from "../../types/service";
 
 const HeroDetail: FC = () => {
+    const [profile, setProfile] = useState<IHeroProfile>(null);
+    const [totalPoint, setTotalPoint] = useState<number>(0);
+
     const { heroId } = useParams();
 
-    return <div>HeroDetail {heroId}</div>;
+    const handleUpdate = () => {
+        updateHeroProfile(heroId, profile)
+            .then((res) => {
+                if (res === "OK") {
+                    alert("Update Success");
+                }
+            })
+            .catch((error) => {
+                if (error?.message) {
+                    alert(error.message);
+                } else {
+                    console.error(error);
+                }
+            });
+    };
+
+    useEffect(() => {
+        getHeroProfile(heroId)
+            .then((heroProfile) => {
+                const heroTotalPoints = Object.keys(heroProfile).reduce(
+                    (acc, cur: keyof IHeroProfile) => {
+                        return acc + heroProfile[cur];
+                    },
+                    0
+                );
+
+                setProfile(heroProfile);
+                setTotalPoint(heroTotalPoints);
+            })
+            .catch((error) => {
+                if (error?.message) {
+                    alert(error.message);
+                } else {
+                    console.error(error);
+                }
+            });
+    }, [heroId]);
+
+    const remainPoints = profile
+        ? totalPoint -
+          Object.keys(profile).reduce((acc, cur: keyof IHeroProfile) => {
+              return acc + profile[cur];
+          }, 0)
+        : 0;
+
+    return (
+        <div style={{ padding: "32px" }}>
+            <h1>HeroDetail {heroId}</h1>
+            <div>
+                <ul>
+                    <li>STR : {profile?.str}</li>
+                    <li>INT : {profile?.int}</li>
+                    <li>AGI : {profile?.agi}</li>
+                    <li>LUK : {profile?.luk}</li>
+                </ul>
+            </div>
+            <p>剩餘點數 : {remainPoints}</p>
+            <button type="button" onClick={handleUpdate}>
+                Update
+            </button>
+        </div>
+    );
 };
 
 export default HeroDetail;
