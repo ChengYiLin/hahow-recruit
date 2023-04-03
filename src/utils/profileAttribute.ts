cbfbb37e8115936c243bc71d9193d1e47bed9b3a
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-04-03 15:20:40 +0800

    doc: Add JSDoc for utils function

--- src/utils/profileAttribute.ts
+++ src/utils/profileAttribute.ts
@@ -1,5 +1,10 @@
 import { IHeroProfile } from "../types/service";
 
+/**
+ * Calculates the total number of points in a hero's profile.
+ * @param {IHeroProfile} profileData - The hero's profile data.
+ * @return {number} - The total number of points in the hero's profile.
+ */
 export const getTotalPoints = (profileData: IHeroProfile): number => {
     if (!profileData || typeof profileData !== "object") {
         return 0;
